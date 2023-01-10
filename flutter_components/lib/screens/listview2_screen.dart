f922b3dd38d9acd14b72f2d05a2c214bc9e9bd70
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-10 16:06:40 -0500

    Tap sobre ListTiles y estilo del AppBar

--- flutter_components/lib/screens/listview2_screen.dart
+++ flutter_components/lib/screens/listview2_screen.dart
@@ -15,12 +15,21 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Center(child: Text("Listview Tipo 2")),
+          elevation: 10,
+          backgroundColor: Colors.indigo,
         ),
         body: ListView.separated(
           itemCount: options.length,
           itemBuilder: (context, index) => ListTile(
             title: Text(options[index]),
-            trailing: const Icon(Icons.arrow_forward_ios_outlined),
+            trailing: const Icon(
+              Icons.arrow_forward_ios_outlined,
+              color: Colors.indigo,
+            ),
+            onTap: () {
+              final game = options[index];
+              print(game);
+            },
           ),
           separatorBuilder: (_, __) => const Divider(),
         ));

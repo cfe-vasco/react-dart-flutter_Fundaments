707c16f917ca7a54d5dd50c7d473e86b0eb0def1
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-10 14:14:17 -0500

    ListView-Mostrar una lista de constantes

---
+++ flutter_components/lib/screens/listview1_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+   
+  const Listview1Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text("Listview Tipo 1")
+        ),
+      ),
+      body: ListView(
+        children: const [
+          Text('Hola mundo'),
+        ],
+      )
+    );
+  }
+}

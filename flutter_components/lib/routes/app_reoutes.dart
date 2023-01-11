84861d0bf198a307b9f2c2de93bbdcecba5d6934
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-10 20:52:46 -0500

    Creacion de metodos con las rutas de la App

--- flutter_components/lib/routes/app_reoutes.dart
+++ flutter_components/lib/routes/app_reoutes.dart
@@ -8,20 +8,51 @@ class AppRoutes {
 
   static final menuOptions = <MenuOption>[
     //TODO: Borrar home
-    MenuOption(route: 'home', name: 'Home Screen', screen: const HomeScreen(), icon: Icons.home),
-    MenuOption(route: 'listview1', name: 'Listview tipo 1', screen: const Listview1Screen(), icon: Icons.list_alt),
-    MenuOption(route: 'listview2', name: 'Listview tipo 2', screen: const Listview2Screen(), icon: Icons.list_alt),
-    MenuOption(route: 'alert', name: 'Alertas', screen: const AlertScreen(), icon: Icons.notifications_active_rounded),
-    MenuOption(route: 'card', name: 'Tarjetas - Cards', screen: const CardScreen(), icon: Icons.card_membership),
+    MenuOption(
+        route: 'home',
+        name: 'Home Screen',
+        screen: const HomeScreen(),
+        icon: Icons.home),
+    MenuOption(
+        route: 'listview1',
+        name: 'Listview tipo 1',
+        screen: const Listview1Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'listview2',
+        name: 'Listview tipo 2',
+        screen: const Listview2Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'alert',
+        name: 'Alertas',
+        screen: const AlertScreen(),
+        icon: Icons.notifications_active_rounded),
+    MenuOption(
+        route: 'card',
+        name: 'Tarjetas - Cards',
+        screen: const CardScreen(),
+        icon: Icons.card_membership),
   ];
 
-  static Map<String, Widget Function(BuildContext)> routes = {
-    'home': (BuildContext context) => const HomeScreen(),
-    'listview1': (BuildContext context) => const Listview1Screen(),
-    'listview2': (BuildContext context) => const Listview2Screen(),
-    'alert': (BuildContext context) => const AlertScreen(),
-    'card': (BuildContext context) => const CardScreen(),
-  };
+  static Map<String, Widget Function(BuildContext)> getAppRoutes() {
+    Map<String, Widget Function(BuildContext)> appRoutes = {};
+
+    for (final option in menuOptions) {
+      appRoutes.addAll({option.route: (BuildContext context) => option.screen});
+    }
+
+    return appRoutes;
+  }
+
+  // Esta manera es igual <MenuOption> de arriba. Tambien se puede usar esta opcion.
+  // static Map<String, Widget Function(BuildContext)> routes = {
+  //   'home': (BuildContext context) => const HomeScreen(),
+  //   'listview1': (BuildContext context) => const Listview1Screen(),
+  //   'listview2': (BuildContext context) => const Listview2Screen(),
+  //   'alert': (BuildContext context) => const AlertScreen(),
+  //   'card': (BuildContext context) => const CardScreen(),
+  // };
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     return MaterialPageRoute(builder: (context) => const AlertScreen());
--- flutter_components/lib/screens/home_screen.dart
+++ flutter_components/lib/screens/home_screen.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/routes/app_reoutes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //Para este ejercicio se puede hacer creando una variable final o llamando por compleato a la clase AppRoutes
+    final menuOptions = AppRoutes.menuOptions;
+
     return Scaffold(
         appBar: AppBar(
           title: const Center(child: Text("Componentes en Flutter")),
         ),
         body: ListView.separated(
             itemBuilder: (context, index) => ListTile(
-                  leading: const Icon(Icons.access_time_outlined),
-                  title: const Text('Nombre de ruta'),
+                  leading: Icon(menuOptions[index].icon, color: Colors.indigo,),
+                  title: Text(menuOptions[index].name),
                   onTap: () {
                     // final route = MaterialPageRoute(
                     //   builder: (context) => const Listview1Screen()
                     // );
                     // Navigator.push(context, route);
 
-                    Navigator.pushNamed(context, 'card2');
+                    Navigator.pushNamed(context, menuOptions[index].route);
                   },
                 ),
             separatorBuilder: (_, __) => const Divider(),
-            itemCount: 100));
+            itemCount: menuOptions.length
+          )
+        );
   }
 }

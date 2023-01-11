0add0e322eb66322d67153acb62cb3fe961ff388
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-11 15:54:10 -0500

    Argumentos a los Widgets

--- flutter_components/lib/screens/card_screen.dart
+++ flutter_components/lib/screens/card_screen.dart
@@ -18,7 +18,11 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10,),
-          CustomCardType2(),
+          CustomCardType2(name: 'Un hermoso paisaje', imageURL: 'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg'),
+          SizedBox(height: 10,),
+          CustomCardType2(name: 'Un segundo hermoso paisaje', imageURL: 'https://www.superprof.co.uk/blog/wp-content/uploads/2018/02/landscape-photography-tutorials.jpg',),
+          SizedBox(height: 10,),
+          CustomCardType2(imageURL: 'https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?w=2000',),
         ],
       )
     );
--- flutter_components/lib/widgets/custom_card_type_2.dart
+++ flutter_components/lib/widgets/custom_card_type_2.dart
@@ -2,33 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_components/themes/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  final String imageURL;
+  final String? name;
+
+  const CustomCardType2(
+      {super.key, required this.imageURL, this.name});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20)
-      ),
-      elevation: 30,
-      shadowColor: AppTheme.primary.withOpacity(0.5),
-      child: Column(
-        children: [
-          const FadeInImage(
-            image: NetworkImage('https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg'), 
-            placeholder: AssetImage('assets/jar-loading.gif'),
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        elevation: 30,
+        shadowColor: AppTheme.primary.withOpacity(0.5),
+        child: Column(children: [
+          FadeInImage(
+            image: NetworkImage(imageURL),
+            placeholder: const AssetImage('assets/jar-loading.gif'),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
 
+          if(name != null)
           Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            child: const Text('Un hermoso paisaje')
-          )
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: Text(name!)
+            )
         ]
       )
     );

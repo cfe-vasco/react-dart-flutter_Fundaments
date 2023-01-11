a58ddd495f128450cbc001d1b9041fa0b76281ed
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-11 13:12:16 -0500

    Card Widget

--- flutter_components/lib/screens/card_screen.dart
+++ flutter_components/lib/screens/card_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/themes/app_theme.dart';
 
 class CardScreen extends StatelessWidget {
    
@@ -6,10 +7,27 @@ class CardScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Card Widget')),
       ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+
+        children: [
+          Card(
+            child: Column(
+              children: const[
+                ListTile(
+                  leading: Icon(Icons.photo_album_outlined, color: AppTheme.primary,),
+                  title: Text('Soy un titulo'),
+                  subtitle: Text('Aliqua do id Lorem amet sunt ex eiusmod irure incididunt. Consectetur deserunt minim officia proident incididunt sunt cillum cupidatat. Eu esse sint eu ut nostrud. Excepteur id sint eu sunt incididunt est ullamco quis. Sunt fugiat anim aliqua elit dolor esse tempor proident sit.'),
+                )
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
 }

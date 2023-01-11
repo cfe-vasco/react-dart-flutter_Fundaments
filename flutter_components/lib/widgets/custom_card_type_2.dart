0fbc88bfb2f74b97bd84a2b7384e123cab90944c
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-11 15:36:29 -0500

    Ajustar el tamaño de la imagen

--- flutter_components/lib/widgets/custom_card_type_2.dart
+++ flutter_components/lib/widgets/custom_card_type_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/themes/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
   const CustomCardType2({super.key});
@@ -6,12 +7,27 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10, 
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20)
+      ),
+      elevation: 30,
+      shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
-        children: const [
-          FadeInImage(
+        children: [
+          const FadeInImage(
             image: NetworkImage('https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg'), 
             placeholder: AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('Un hermoso paisaje')
           )
         ]
       )

86ae0a15d57d223ab64c25759b1bcfe76b7e58b6
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-12 20:13:51 -0500

    Checkbox y Switches

--- flutter_components/lib/screens/slider_screen.dart
+++ flutter_components/lib/screens/slider_screen.dart
@@ -10,6 +10,7 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnable = true;
 
   @override
   Widget build(BuildContext context) {
@@ -17,29 +18,65 @@ class _SliderScreenState extends State<SliderScreen> {
         appBar: AppBar(
           title: const Text('Slider & Checks'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Slider(
+        body: Column(
+          children: [
+            Slider(
                 min: 50,
                 max: 400,
                 // Da las divisiones al Slider de valor en valor que se especifique
                 // divisions: 10,
                 activeColor: AppTheme.primary,
                 value: _sliderValue,
-                onChanged: (value) {
-                  _sliderValue = value;
-                  setState(() {});
-                },
+                onChanged: _sliderEnable
+                    ? (value) {
+                        _sliderValue = value;
+                        setState(() {});
+                      }
+                    : null),
+
+            // Checkbox(
+            //   value: _sliderEnable,
+            //   onChanged: (value) {
+            //     _sliderEnable = value ?? true;
+            //     setState(() {});
+            //   },
+            // ),
+
+            // Switch(
+            //     value: _sliderEnable,
+            //     onChanged: (value) => setState(() {
+            //           _sliderEnable = value;
+            //         })),
+
+            CheckboxListTile(
+                activeColor: AppTheme.primary,
+                title: const Text('Habilitar Slider'),
+                value: _sliderEnable,
+                onChanged: (value) => setState(() {
+                      _sliderEnable = value ?? true;
+                    })),
+
+            SwitchListTile(
+                activeColor: AppTheme.primary,
+                title: const Text('Habilitar Slider'),
+                value: _sliderEnable,
+                onChanged: (value) => setState(() {
+                      _sliderEnable = value;
+                    })),
+            
+            const AboutListTile(),
+
+            Expanded(
+              child: SingleChildScrollView(
+                child: Image(
+                  image: const NetworkImage(
+                      'https://static.wikia.nocookie.net/death-battle-fanon-wiki-en-espanol/images/5/59/Fcf46f73f864493aaf959b732d1605ce.png/revision/latest?cb=20190821193916&path-prefix=es'),
+                  fit: BoxFit.contain,
+                  width: _sliderValue,
+                ),
               ),
-              Image(
-                image: const NetworkImage('https://static.wikia.nocookie.net/ficcion-sin-limites/images/8/88/DP.png/revision/latest?cb=20210217170751&path-prefix=es'),
-                fit: BoxFit.contain,
-                width: _sliderValue,
-              ),
-              const SizedBox(height: 50,)
-            ],
-          ),
+            ),
+          ],
         ));
   }
 }

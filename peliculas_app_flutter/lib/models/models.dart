50d11cf429c4992cbf9a1d5d515c988e8f731b7c
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-13 18:43:27 -0500

    Películas populares

--- peliculas_app_flutter/lib/models/models.dart
+++ peliculas_app_flutter/lib/models/models.dart
@@ -1,2 +1,3 @@
+export 'package:peliculas_app_flutter/models/popular_response.dart';
 export 'package:peliculas_app_flutter/models/now_playing_response.dart';
 export 'package:peliculas_app_flutter/models/movie.dart';
--- peliculas_app_flutter/lib/providers/movies_provider.dart
+++ peliculas_app_flutter/lib/providers/movies_provider.dart
@@ -8,10 +8,12 @@ class MoviesProvider extends ChangeNotifier {
   final String _lenguaje = 'es-ES';
 
   List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
 
   MoviesProvider() {
     print('MoviesProvider incializado');
     getOnDisplayMovies();
+    getPopularMovies();
   }
 
   getOnDisplayMovies() async {
@@ -24,6 +26,19 @@ class MoviesProvider extends ChangeNotifier {
     final nowPlayigResponse = NowPlayingResponse.fromJson(response.body);
 
     onDisplayMovies = nowPlayigResponse.results;
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    var url = Uri.https(_baseURL, '3/movie/popular', {
+      'api_key': _apiKey_,
+      'language': _lenguaje,
+      'page': '1',
+    });
+    final response = await http.get(url);
+    final popularResponse = PopularResponse.fromJson(response.body);
+
+    popularMovies = [...popularMovies, ...popularResponse.results];
     notifyListeners();
   }
 }

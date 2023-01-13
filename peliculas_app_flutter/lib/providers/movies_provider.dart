3bae26304961a659d561e3fe0ca0a274a8d1c503
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-13 17:23:52 -0500

    Peticiones HTTP

--- peliculas_app_flutter/lib/providers/movies_provider.dart
+++ peliculas_app_flutter/lib/providers/movies_provider.dart
@@ -1,12 +1,26 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class MoviesProvider extends ChangeNotifier{
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MoviesProvider extends ChangeNotifier {
+  String _apiKey_ = 'fc717f1e907ebd61dcb318d2949c503c';
+  String _baseURL = 'api.themoviedb.org';
+  String _lenguaje = 'es-ES';
+
   moviesProvider() {
     print('MoviesProvider incializado');
     getOnDisplayMovies();
   }
 
-  getOnDisplayMovies() async{
-    print('getOnDisplayMovies');
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseURL, '3/movie/now_playing', {
+      'api_key': _apiKey_,
+      'language': _lenguaje,
+      'page': '1',
+    });
+    final response = await http.get(url);
+    final Map<String, dynamic> dedocedData = json.decode(response.body);
+    print(dedocedData['results']);
   }
 }

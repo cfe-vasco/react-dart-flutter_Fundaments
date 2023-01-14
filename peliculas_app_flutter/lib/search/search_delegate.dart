173d3f0cba18b034fffd46824f3cc61fb6d6c837
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-14 14:22:44 -0500

    Mostrar las películas basado en el query

--- peliculas_app_flutter/lib/search/search_delegate.dart
+++ peliculas_app_flutter/lib/search/search_delegate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:peliculas_app_flutter/models/models.dart';
+import 'package:peliculas_app_flutter/providers/movies_provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -29,17 +32,62 @@ class MovieSearchDelegate extends SearchDelegate {
     return const Text('buildResults');
   }
 
+  Widget _emptyContainer() {
+    return const Center(
+      child: Icon(
+        Icons.movie_creation_outlined,
+        color: Colors.black38,
+        size: 130,
+      ),
+    );
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
     if (query.isEmpty) {
-      return const Center(
-        child: Icon(
-          Icons.movie_creation_outlined,
-          color: Colors.black38,
-          size: 130,
-        ),
-      );
+      return _emptyContainer();
     }
-    return Container();
+
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: ((_, AsyncSnapshot<List<Movie>> snapshot) {
+        if (!snapshot.hasData) return _emptyContainer();
+
+        final movies = snapshot.data!;
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (_, index) => _MovieItem(movie: movies[index]),
+        );
+      }),
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: FadeInImage(
+          placeholder: const AssetImage('assets/no-image.jpg'),
+          image: NetworkImage(movie.fullPosterImg),
+          width: 50,
+          fit: BoxFit.contain,
+        ),
+        title: Text(movie.title),
+        subtitle: Text(movie.originalTitle),
+        onTap: (() {
+          Navigator.pushNamed(
+            context, 'details', 
+            arguments: movie
+          );
+        }
+      )
+    );
   }
 }

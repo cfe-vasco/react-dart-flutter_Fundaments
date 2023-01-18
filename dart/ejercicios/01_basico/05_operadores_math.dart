53b634106bfb1d2daf0d27149393c64dd28236b4
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-18 16:02:51 -0500

    Operadores aritméticos

---
+++ dart/ejercicios/01_basico/05_operadores_math.dart
@@ -0,0 +1,15 @@
+main() {
+  int a = 10 + 5; // + 15
+  a = 20 - 10; //- 10
+  a = 10 * 2; // * 20
+  double b = 10 / 2; // / 5
+  b = 10 % 3; // 1 El sobrante de la division
+  b = -b; // -expr Es usado para cambiar el valor de la expreion
+  int c = 10 ~/ 3; // ~/ 3 Division comun y corriente
+
+  int d = 1;
+  d++; // ++ 2
+  d--; // -- 1
+  d += 2; // += 3 
+  d -= 2; // -= 1 
+}

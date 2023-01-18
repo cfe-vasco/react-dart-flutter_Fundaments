d3c687f73fc8b2fafc23e73f4431810bb0baadf5
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-18 16:15:32 -0500

    Operadores de asignación, condicional, relacional y de tipo

---
+++ dart/ejercicios/01_basico/06_operadores.dart
@@ -0,0 +1,10 @@
+mian() {
+  // Operadores de asignacion
+  int a = 10;
+  int b = 1;
+
+  // Operadores condicionales
+  int c = 23;
+  String resp = c > 25 ? 'C es mayor a 25' : 'C es menor a 25';
+
+}

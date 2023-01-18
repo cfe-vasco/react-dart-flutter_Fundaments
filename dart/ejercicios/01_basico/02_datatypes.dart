ae9699ebac24c6caf4edf5bfb675719ee1dc3730
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-18 13:26:52 -0500

    Tipado de datos: String

--- dart/ejercicios/01_basico/02_datatypes.dart
+++ dart/ejercicios/01_basico/02_datatypes.dart
@@ -1,7 +1,22 @@
+// ====== Números
 void main() {
   int a = 10;
   double b = 5.5;
   int? c;
 
-  print(c);
+  // print(c);
+
+// ====== Strings
+
+  String name = 'Tony';
+  String lastName = 'Stark';
+  String name3 = "O'Connor";
+  String completeName = '$name $lastName';
+
+  String multiLinea = '''
+  Hola mundo. 
+  $completeName
+  Como estas?''';
+
+  print(multiLinea);
 }

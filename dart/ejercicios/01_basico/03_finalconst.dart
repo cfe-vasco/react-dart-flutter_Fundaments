43ca2c7b39840fb245ce4887b2ff518d26ee0b00
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-18 15:43:13 -0500

    Comentarios en Dart

--- dart/ejercicios/01_basico/03_finalconst.dart
+++ dart/ejercicios/01_basico/03_finalconst.dart
@@ -3,18 +3,23 @@ void main() {
   final double b = 10;
   const double c = 10;
 
+  late final double x;
+  x = 10;
+
+  print(x);
+
   // a = 20;
   // b = 20;
   // c = 20;
 
   // final personasFinal = ['Juan', 'Pedro', 'Fernando'];
   // const personasConst = ['Juan', 'Pedro', 'Fernando'];
-  
+
   final List<String> personasFinal = ['Juan', 'Pedro', 'Fernando'];
   const List<String> personasConst = ['Juan', 'Pedro', 'Fernando'];
 
   personasFinal.add('Maria');
   // personasConst.add('Maria');
 
-  print(personasConst);
+  // print(personasConst);
 }
---
+++ dart/ejercicios/01_basico/04_comentarios.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  // Esta es una variable temporal que quiero definir
+  int a = 10; //A deberia de ser 10
+
+  final persons = [
+    // Esta es la primera persona
+    'Fernando', //[0]
+
+    // Esta es la segunda persona
+    'Pedro', //[0]
+
+    // Esta es la tercera persona
+    'Carlos' //[0]
+  ];
+}

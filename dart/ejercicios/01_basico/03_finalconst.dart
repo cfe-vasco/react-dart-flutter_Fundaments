080ef5434ca397c9d801e6f42adead7cbca2b524
CHRISTOPHER FERNANDO VASCO ESPIN <cfe.vasco@yavirac.edu.ec>
2023-01-18 15:21:10 -0500

    Variables, Constantes y Final

---
+++ dart/ejercicios/01_basico/03_finalconst.dart
@@ -0,0 +1,20 @@
+void main() {
+  var a = 10;
+  final double b = 10;
+  const double c = 10;
+
+  // a = 20;
+  // b = 20;
+  // c = 20;
+
+  // final personasFinal = ['Juan', 'Pedro', 'Fernando'];
+  // const personasConst = ['Juan', 'Pedro', 'Fernando'];
+  
+  final List<String> personasFinal = ['Juan', 'Pedro', 'Fernando'];
+  const List<String> personasConst = ['Juan', 'Pedro', 'Fernando'];
+
+  personasFinal.add('Maria');
+  // personasConst.add('Maria');
+
+  print(personasConst);
+}
